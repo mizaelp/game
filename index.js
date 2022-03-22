c7f1c581ceea9447f522c1248a6d5b0f3c0b3619
Mizael <mizaeludi@outlook.com>
2022-03-21 21:36:38 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const res = require('express/lib/response')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server)
+const port = process.env.PORT || 5000 
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', (socket) => {
+  socket.on('message', (msg) => {
+    console.log(msg)
+    io.emit('message', msg)
+    console.log(msg)
+  })
+})
+
+server.listen(3000, () => {
+  console.log('listening on 3000')
+})
